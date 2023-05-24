555d86972f3d757a78726291fcdcaeb289d30db8
Noel Espino Córdova <github@ednoesco.com>
2023-05-23 22:00:04 -0600

    Add Berkeley font and global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
+import localFont from "next/font/local";
 import { Metadata } from "next";
 import "./globals.css";
+
+const berkeley = localFont({
+  display: "swap",
+  src: "./BerkeleyMono-Regular.woff2",
+});
 
 export const metadata: Metadata = {
   title: "Tweets",
@@ -12,7 +18,7 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en-US">
+    <html lang="en-US" className={berkeley.className}>
       <body>{children}</body>
     </html>
   );
---
+++ lib/tweets.ts
@@ -0,0 +1,19 @@
+const ids = "1509045938890887168,1423433602142986245,1451004781154078725";
+
+export async function getTweets(): Promise<any[] | string> {
+  const { API_URL, API_TOKEN } = process.env;
+
+  if (API_URL === undefined || API_TOKEN === undefined) {
+    return "Bad request";
+  }
+
+  const url = new URL(API_URL);
+  url.searchParams.set("ids", ids);
+
+  const response = await fetch(url, {
+    next: { revalidate: 60 },
+    headers: { Authorization: API_TOKEN },
+  });
+
+  return response.json();
+}
